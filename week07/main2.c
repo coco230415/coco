25b5913e00afecf081c9e13308cb9bedb817e6d3
coco230415 <es020608@sookmyung.ac.kr>
2024-10-19 16:24:48 +0900

    7

---
+++ week07/main2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int sumTwo(int a, int b)
+{
+	int result;
+	result = a+b;
+	 	
+	return result;
+}
+
+int square(int n)
+{
+	return n*n;
+}
+
+int get_max(int x, int y)
+{ 
+   if(x > y) return x;
+   else return y;
+}
+
+int main (void) {
+	
+	int num1,num2;
+	
+	printf("Enter the integers: ");
+	scanf("%i %i", &num1, &num2);
+	
+	printf("Result - Sum: %i, Square: %i, Max: %i", sumTwo(num1, num2), square(num1), get_max(num1,num2));
+	
+	return 0;
+}
